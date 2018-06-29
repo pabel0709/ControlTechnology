bf208f07ec815c42171cb9c761e07ec04601fd80
piter1316 <piter_1316@wp.pl>
2018-06-29 10:02:38 +0200

    changed maps zoom

--- js/myJS.js
+++ js/myJS.js
@@ -1,9 +1,8 @@
 function myMap() {
-  var loc = {lat: 50.010648, lng:  20.962189};
+  var loc = {lat: 50.0067129, lng:  20.9623298};
   var map = new google.maps.Map(
-      document.getElementById('googleMap'), {zoom: 15, center: loc});
-  var marker = new google.maps.Marker({position: loc, map: map});
-  var content= 'ascsdvsVd'
+      document.getElementById('googleMap'), {zoom: 18, center: loc});
+
   var infowindow = new google.maps.InfoWindow({content: contentString});
   marker.addListener('click', function() {
           infowindow.open(map, marker);
