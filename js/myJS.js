b1e2ecda3c400df43e4ee78aae3c34152132bd00
piter1316 <piter_1316@wp.pl>
2018-06-29 09:45:45 +0200

    googleMaps with API key better fix modal opacity

--- js/myJS.js
+++ js/myJS.js
@@ -1,8 +1,13 @@
 function myMap() {
   var loc = {lat: 50.010648, lng:  20.962189};
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 15, center: loc});
+      document.getElementById('googleMap'), {zoom: 15, center: loc});
   var marker = new google.maps.Marker({position: loc, map: map});
+  var content= 'ascsdvsVd'
+  var infowindow = new google.maps.InfoWindow({content: contentString});
+  marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
 }
 
 window.onscroll = function() {scrollFunction()};
