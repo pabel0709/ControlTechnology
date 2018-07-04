14a858a801119943fff1a54ed17594495e8583e7
piter1316 <piter_1316@wp.pl>
2018-07-04 09:18:34 +0200

    animation from top only

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -38,16 +38,44 @@ $(window).scroll(function() {
 //     $('div .aa').addClass('abcd');
 //   }
 // });
+var homeHeight = $("#home").height();
+var aboutHeight = $("#about").height();
+var galleryHeight = $("#gallery").height();
+var contactHeight = $("#contact").height();
+var mapHeight = $("#map").height();
+
 $(window).scroll(function() {
-  if ($(this).scrollTop() > 300) {
-    $('div').addClass('visible');
-  } else {
-    $('div .aa').removeClass('visible');
+  if ($(this).scrollTop() > homeHeight) {
+    $('div .hiddAbout').addClass('visible');
+  } else if($(this).scrollTop() == 0) {
+    $('div .hiddAbout').removeClass('visible');
   }
 });
-
-// $(window).scroll(function() {
-//   if ($(this).scrollTop() < 400) {
-//     $('aa').addClass('abcd');
-//   }
-// });
+$(window).scroll(function() {
+  if ($(this).scrollTop() > homeHeight + aboutHeight + 300) {
+    $('div .hiddServices').addClass('visible');
+  } else if($(this).scrollTop() == 0) {
+    $('div .hiddServices').removeClass('visible');
+  }
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > homeHeight + aboutHeight + galleryHeight + 300 ) {
+    $('div .hiddGallery').addClass('visible');
+  } else if($(this).scrollTop() == 0) {
+    $('div .hiddGallery').removeClass('visible');
+  }
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > homeHeight + aboutHeight + galleryHeight + contactHeight +300) {
+    $('div .hiddContact').addClass('visible');
+  } else if($(this).scrollTop() == 0) {
+    $('div .hiddContact').removeClass('visible');
+  }
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > homeHeight + aboutHeight + galleryHeight + contactHeight + mapHeight +300) {
+    $('div .hiddMap').addClass('visible');
+  } else if($(this).scrollTop() == 0) {
+    $('div .hiddMap').removeClass('visible');
+  }
+});
